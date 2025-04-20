b5e0c0388be4f01406a39d82c550927eafb319dc
amil-lab <amilmemmedli944@gmail.com>
2025-04-20 15:29:33 -0400

    whois322e

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -v out="$1.csv" 'BEGIN{FS=": ";OFS=","} /^(Registrant|Admin|Tech) (Organization|State\/Province|Country|Email):/ {gsub(/\r/,"",$2); print $1,$2 >> out}'
+whois "$1" | awk -v out="$1.csv" 'BEGIN{FS=": ";OFS=","} /^(Registrant|Admin|Tech) (Name|Organization|Street|City|State\/Province|Postal Code|Country|Phone|Phone Ext|Fax|Fax Ext|Email):/ {gsub(/\r/, "", $2); print $1, $2 >> out}'
