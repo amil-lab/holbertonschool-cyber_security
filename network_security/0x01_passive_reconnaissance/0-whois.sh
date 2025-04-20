7b1bcbfad49a437e818dd11d5f36badf5862b3f1
amil-lab <amilmemmedli944@gmail.com>
2025-04-20 15:06:00 -0400

    whois1

---
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+whois "$1" | awk 'BEGIN{print "Type,Name,Org,Email,Phone"} /Registrant|Admin|Tech/ {t=$1} /Name:|Organization:|Email:|Phone:/ {gsub(/\r/,""); f[$1]=$2} /^$/ {if(t){print t","f["Name:"]","f["Organization:"]","f["Email:"]","f["Phone:"]; delete f; t=""}}'
