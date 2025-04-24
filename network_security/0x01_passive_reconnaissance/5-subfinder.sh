9d38bd99588ae66d3e4d2adaf98029abeece9af9
amil-lab <amilmemmedli944@gmail.com>
2025-04-24 14:50:01 -0400

    subfinderweqdfsat

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
-#!/bin/bash  
-subfinder -silent -d$1 -nW -oI -o$1.txt  
+#!/bin/bash
+subfinder -silent -d $1 | while read sub; do host "$sub" | awk '/has address/ {gsub(/\.$/,"",$1); print $1 "," $4}'; done > "${1}.txt"
